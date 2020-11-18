52d7612bca19c56dd033912174e9f156cc804486
Seokbin Yoon <nyoongoon@naver.com>
2020-11-18 18:38:00 +0900

    after_반복되는 수열에 대해 파악(입력을 제대로 받지 못함, 틀렸음)

--- 3_2_Greedy_큰_수의_법칙.py
+++ 3_2_Greedy_큰_수의_법칙.py
@@ -6,15 +6,12 @@ arr.sort()
 first = arr[n-1] # 가장 큰 수
 second = arr[n-2] # 가장 큰 수
 
-while True:
-    for i in range(k): # 가장 큰 수를 k번 더하기 # k-1번이 아니라 k번!
-        if m == 0: # 더할 때마다 m 값 검사
-            break
-        result += first
-        m -= 1 # 더할 때마다 1 빼기
-    if m == 0: # 더할 때마다 m 값 검사
-        break
-    result += second
-    m -= 1 # 더할 때마다 1 빼기
+# 반복되는 수열에 대해 파악!!!
+count = int(m / k+1) * k
+count += m % (k+1)
 
+result = 0
+result += (count) * first # 가장 큰 수 더하기
+result += (m - count) * second
 print(result)
+
