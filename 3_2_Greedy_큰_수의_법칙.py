12c7fd26ebe6788b061a0e8e67925c2d181e4498
Seokbin Yoon <nyoongoon@naver.com>
2020-11-18 19:28:06 +0900

    after_반복되는 수열에 대해 파(내가 만든 공식으로 푼 문제)

--- 3_2_Greedy_큰_수의_법칙.py
+++ 3_2_Greedy_큰_수의_법칙.py
@@ -4,14 +4,11 @@ result = 0 # 결과값
 # 최댓값 뽑기 -> 오름차순 정렬 ->(작은 수가 먼저)반대로 세기!!
 arr.sort()
 first = arr[n-1] # 가장 큰 수
-second = arr[n-2] # 가장 큰 수
+second = arr[n-2] # 두번쨰 큰 수
+# 내가 구한 공식!!
+result = (k*first+second) * (m // (k + 1))
+        # 한 수열내의 합 * 수열의 갯수
+result += (m % (k + 1)) * first
+        # 나머지 값 더하기
 
-# 반복되는 수열에 대해 파악!!!
-count = int(m / k+1) * k
-count += m % (k+1)
-
-result = 0
-result += (count) * first # 가장 큰 수 더하기
-result += (m - count) * second
 print(result)
-
