6b5e0ee3ecbe8fe6fdda17615414ac4d025c7f00
Seokbin Yoon <nyoongoon@naver.com>
2020-11-18 16:12:05 +0900

    after

--- 3_1_Greedy_거스름돈.py
+++ 3_1_Greedy_거스름돈.py
@@ -1,6 +1,5 @@
+"""
 n = int(input()) # 거슬러 줘야할 돈
-
-
 def count(n):
     coin = 0
     while n >= 500:
@@ -16,4 +15,14 @@ def count(n):
         n -= 10
         coin += 1
     return coin
+"""
 
+n = 1260 # 예시
+count = 0
+# 큰 단위의 화폐부터 차례대로 확인
+coin_types = [500, 100, 50, 10]
+
+for coin in coin_types:
+    count += n // coin # 정수 나누기 후 갯수 카운트에 저장
+    n %= coin # n에 n을 coin으로 나눈 나머지 저장
+print(count)
