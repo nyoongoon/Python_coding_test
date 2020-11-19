4dc5f0c5b8d185942d1bcf24cc833be5bdb7bd18
Seokbin Yoon <nyoongoon@naver.com>
2020-11-19 13:11:07 +0900

    after_내가 푼 정답+답안

--- 3_3_Greedy_숫자_카드_게임.py
+++ 3_3_Greedy_숫자_카드_게임.py
@@ -15,4 +15,30 @@ print(max)
 
 
 # 각 행의 최솟값은 arr[i][-1]
+"""
+n, m = map(int, input().split())
+result = 0
+# 한 줄씩 입력받아 확인
+for i in n:
+    arr = list(map(int, input().split()))
+    # 현재 줄에서 가장 작은 수 찾기
+    min_value = min(arr)
+    # 가장 작은 수들 중에서 가장 큰 수 찾기
+    result = max(result, min_value)
+print(result)
+"""
+"""
+n, m = map(int, input().split())
+result = 0
+# 한 줄씩 입력받아 확인
+for i in n:
+    arr = list(map(int, input().split()))
+    # 현재 줄에서 가장 작은 수 찾기
+    min_value = 10001
+    for a in arr:
+        min_value = min(min_value, a) # 각 행에서 최소값 뽑아서 저장
+    # 가장 작은 수들 중에서 가장 큰 수 찾기
+    result = max(result, min_value)
+print(result)        
+"""
 
