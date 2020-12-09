58e9741d915e2848a8cbfceebc56a2f9c1a1ad99
Seokbin Yoon <nyoongoon@naver.com>
2020-12-09 18:53:11 +0900

    after_ 틀림(왜 틀렸는지 모르겠음), 답안 예시

--- 4_4_Imple_게임 개발.py
+++ 4_4_Imple_게임 개발.py
@@ -1,60 +1,64 @@
+# N, M을 공백으로 구분하여 입력받기
 n, m = map(int, input().split())
-a, b, d = map(int, input().split())
 
-arr = []
+# 방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
+# 2차원 리스트를 만들 때는 컴프리헨션을 이용하는 것이 효과적
+# 반복을 위한 변수 무시->_(언더바), [0] 곱하기 m -> 열의 갯수(!=길이)
+d = [[0] * m for _ in range(n)]
+
+# 현재 캐릭터의 X좌표, Y좌표, 방향을 입력받기
+x, y, direction = map(int, input().split())
+d[x][y] = 1 # 현재 좌표 방문 처리
+
+# 전체 맵 정보를 입력받기
+array = []
 for i in range(n):
-    arr.append([])
-    for j in range(m):
-        arr[i].extend(list(map(int, input().split())))
+    # 리스트에 리스트를 append() 가능.
+    array.append(list(map(int, input().split())))
 
-count = 0
+# 북, 동, 남, 서 방향 정의
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
-while(True):
-    if not arr[a-1][b] == 0 or arr[a][b-1] == 0 or arr[a+1][b] == 0 or arr[a][b+1] == 0:
-        if d == 0:
-            b = b + 1
-            if arr[a][b] == 1:
-                break
-        elif d == 1:
-            a = a - 1
-            if arr[a][b] == 1:
-                break
-        elif d == 2:
-            b = b - 1
-            if arr[a][b] == 1:
-                break
-        elif d == 3:
-            a = a + 1
-            if arr[a][b] == 1:
-                break
+# 왼쪽으로 회전
+def turn_left():
+    # 전역변수는 함수 내에서 global로 받는다
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction = 3
 
-    if d == 0:
-        if arr[a-1][b] == 0:
-            arr[a][b] = 2
-            a = a-1
-            count += 1
-            d = 3
-        else: d = 3
-    elif d == 1:
-        if arr[a][b-1] == 0:
-            arr[a][b] = 2
-            b = b-1
-            count += 1
-            d = 0
-        else: d = 0
-    elif d == 2:
-        if arr[a+1][b] == 0:
-            arr[a][b] = 2
-            a = a+1
-            count += 1
-            d = 1
-        else: d = 1
-    elif d == 3:
-        if arr[a][b+1] == 0:
-            arr[a][b] = 2
-            b = b+1
-            d = 2
-            count += 1
-        else: d = 2
+# 시뮬레이션 시작
+count = 1
+turn_time = 0
+
+while True:
+    # 왼쪽으로 회전
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    # 회전한 이후 정면에 가보지 않은 칸이 존재하는 경우 이동
+    if d[nx][ny] == 0 and array[nx][ny] == 0:
+        d[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0 # 턴타임 초기화
+        continue
+    # 회전한 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
+    else:
+        turn_time += 1
+    # 네 방향 모두 갈 수 없는 경우
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        # 뒤로 갈 수 있다면 이동하기(육지라면 이동하기)
+        if array[nx][ny] == 0:
+            x = nx
+            y = ny
+        # 뒤가 바다로 막혀있는 경우
+        else:
+            break
+        turn_time = 0
 
 print(count)
