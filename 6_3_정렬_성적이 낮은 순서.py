3518e4d176c4329751ffe5bfa2d9476fd3acae59
Seokbin Yoon <nyoongoon@naver.com>
2020-12-21 18:54:44 +0900

    after_ 틀림, 답안예시

--- 6_3_정렬_성적이 낮은 순서.py
+++ 6_3_정렬_성적이 낮은 순서.py
@@ -1,13 +1,14 @@
 n = int(input())
 arr = []
 for i in range(n):
-    arr.append(tuple(str(input().split())))
+    input_data = input().split()
+    arr.append(((input_data[0], int(input_data[1]))))
 
 def setting(arr):
     return arr[1]
 
-result = sorted(arr, key=setting, reverse=True)
-
-for i in range(n):
-    print(arr[0])
+result = sorted(arr, key=setting)
+# 람다 가능 result = sorted(arr, key = lambda setting:setting[1])
+for results in result:
+    print(results[0], end=' ')
 
